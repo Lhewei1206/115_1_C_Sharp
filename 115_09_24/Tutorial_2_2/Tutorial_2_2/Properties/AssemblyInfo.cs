78c26edd98e6075ccda439bd2d90a69a05d90d9e
Lhewei1206 <a115222038@mail.shu.edu.tw>
2026-09-24 17:25:39 +0800

    20260924

--- 115_09_24/Tutorial_2_2/Tutorial_2_2/Properties/AssemblyInfo.cs
+++ 115_09_24/Tutorial_2_2/Tutorial_2_2/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// 組件的一般資訊是由下列的屬性集 
-// 控制。變更這些屬性值可修改與組件關聯的
+// 組件的一般資訊是由下列的屬性集控制。
+// 變更這些屬性的值即可修改組件的相關
 // 資訊。
 [assembly: AssemblyTitle("Tutorial_2_2")]
 [assembly: AssemblyDescription("")]
@@ -14,22 +14,20 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// 將 ComVisible 設定為 false 會使得這個組件中的型別 
-// 對 COM 元件而言為不可見。如果您需要從 COM 存取此組件中 
-// 的型別，請在該型別上將 ComVisible 屬性設定為 true。
+// 將 ComVisible 設為 false 可對 COM 元件隱藏
+// 組件中的類型。若必須從 COM 存取此組件中的類型，
+// 的類型，請在該類型上將 ComVisible 屬性設定為 true。
 [assembly: ComVisible(false)]
 
 // 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
-[assembly: Guid("d38c1f84-5b46-4eb2-96a8-0c4f4c528922")]
+[assembly: Guid("9713e2e8-8638-477c-9554-e8a1a49e483c")]
 
-// 組件的版本資訊是由下列四項值構成:
+// 組件的版本資訊由下列四個值所組成: 
 //
 //      主要版本
-//      次要版本 
+//      次要版本
 //      組建編號
 //      修訂編號
 //
-// 您可以指定所有的值，也可以依照以下的方式，使用 '*' 將修訂和組建編號 
-// 指定為預設值:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
